44a31ac09ec407229d36bcad5cde94bd4404cdbf
Rohit Chitampalle <chitampalle813@gmail.com>
2024-02-09 11:55:13 +0530

    book apis post and get list of books

--- auth_process/src/Routes/routes.js
+++ auth_process/src/Routes/routes.js
@@ -1,7 +1,7 @@
 import {createBrowserRouter} from "react-router-dom";
 import SignUp from "../components/auth/SignUp";
 import Login from "../components/auth/Login";
-import User from "../components/user/User.jsx"
+import User from "../components/user/User"
 const router = createBrowserRouter(
 [
 
@@ -13,7 +13,7 @@ const router = createBrowserRouter(
     path:"/login",
     element:<Login/>
 },{
-    path:"/user/:id",
+    path: "/user/bookList/:id",
     element:<User/>
 }
 
--- backend_auth/src/routes/book.js
+++ backend_auth/src/routes/book.js
@@ -1,11 +1,11 @@
 const express=require('express');
 
-const { handleGetAllBookList } = require('../controllers/books');
+const { handleGetAllBookList, handleSetBooks, handleGetBooksById } = require('../controllers/books');
 const upload = require('../middlewares/upload_mutler');
 
 const router =express.Router();
 
 
-router.get("/book_list",upload.any(), handleGetAllBookList)
+router.get("/book_list", upload.any(), handleGetAllBookList).get("/user/bookList/:id", upload.any(), handleGetBooksById).post('/set/user/books', upload.any(), handleSetBooks)
 
 module.exports=router
